6efdf30557fa352e4495f8e31bcf265b323a3893
barrettMCW <103449618+barrettMCW@users.noreply.github.com>
2022-10-03 14:19:31 +0000

    Add files via upload

---
+++ install-plugins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Takes a file of tsv (title,description,url) values and adds (war) files to plugin folder
+TSV=$1
+if [[ ! -f $TSV ]]; then
+    echo "$TSV does not exist"
+    exit 1
+fi
+# awk out curl commands based on tsv
+# execute compiled commands
+awk -v XNAT_HOME=$XNAT_HOME -F '\t' '{if ($1 != "TITLE") {
+    print "curl -Lo" XNAT_HOME "/plugins/"$1 ".jar " $3
+}}' $TSV | tac | while read line; do
+    $line
+done 
