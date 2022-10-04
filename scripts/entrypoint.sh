4436162b42c40444c48c38f96fc746976d4503df
barrettMCW <mjbarrett@mcw.edu>
2022-10-04 11:04:28 -0500

    skip 80 level scripts

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -e
-[[ -z  $(ls $XNAT_HOME/logs) ]] && INIT=1
+[[ -e  $XNAT_HOME/logs/init.byte ]] && INIT=1
 for f in /startup/*; do
     if [ -f "$f" -a -x "$f" ]; then
 	    #if it is an 80 level script we only run it on a fresh container
-        [[ "$f" =~ 8[0-9]-[a-zA-Z]+\.[a-zA-Z]+ ]] && \
+        echo $(basename $f)
+        [[ $(basename $f) =~ 8[0-9]-[a-zA-Z\-]+\.[a-zA-Z]+ ]] && \
 		[[ $INIT == 1 ]] && continue
 	    echo "Running $f $@"
         "$f" "$@"
