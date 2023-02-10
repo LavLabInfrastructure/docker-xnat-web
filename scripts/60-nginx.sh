3743db5d8c3f547e9f070b90965aa8a7fb29d78b
barrettMCW <mjbarrett@mcw.edu>
2023-02-10 02:28:52 -0600

    nginx conf in compose

--- scripts/60-nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# if nginx configs, move to volume
-mkdir -p /etc/nginx/conf.d/xnat
-if [[ "$(ls -A /nginx)" ]]; then
-    cp /nginx/* /etc/nginx/conf.d/xnat
-else
-    echo "ERROR: No nginx conf provided! Using default http config."
-    cp /tmp/nginx-http.conf /etc/nginx/conf.d/xnat/nginx-http.conf
-fi
